fbda2bcbb1404d5b0b073cbeff50b8c3b220f4bc
Jeswin-w <jeswinw07@gmail.com>
2025-05-31 22:52:20 +0530

    sad orphan commit

---
+++ src/main/java/com/saharaj/bootsocial/BootsocialApplication.java
@@ -0,0 +1,13 @@
+package com.saharaj.bootsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saharaj/bootsocial/BootsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saharaj.bootsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
