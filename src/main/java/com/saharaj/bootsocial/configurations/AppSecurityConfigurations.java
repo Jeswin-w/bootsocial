c49ab658e84f7a0871845cc2d625d272616163dd
Jeswin-w <jeswinw07@gmail.com>
2025-06-04 14:01:22 +0530

    db connection

---
+++ src/main/java/com/saharaj/bootsocial/configurations/AppSecurityConfigurations.java
@@ -0,0 +1,15 @@
+package com.saharaj.bootsocial.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppSecurityConfigurations {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        //TODO: implement CSRF and XSS security config
+//        return http.
+}
